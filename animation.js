369ac156c0c2adbb7ad19e6d85650a82eb30b18b
rishirocks <127014244+rishirocks@users.noreply.github.com>
2023-09-06 21:28:25 +0530

    Update animation.js

--- animation.js
+++ animation.js
@@ -29,10 +29,53 @@ window.onload = function() {
   }
 };
 
-
 function addCrossLine(cell) {
   cell.classList.toggle('cross-line');
 }
 
+// To-do-list
+document.addEventListener('DOMContentLoaded', function(){
 
-  
+let button = document.getElementById('add');
+let todolist = document.getElementById('todoList');
+let input = document.getElementById('input');
+
+// Local Storage
+let todos = [];
+window.onload = ()=>{
+  todos = JSON.parse(localStorage.getItem('todos')) || []
+  todos.forEach(todo =>addtodo(todo))  
+}
+
+button.addEventListener('click',()=>{
+  todos.push(input.value)
+  localStorage.setItem('todos',JSON.stringify(todos))
+  console.log(todos)
+  addtodo(input.value)
+  input.value = ''
+});
+
+function addtodo(todo) {
+  let para = document.createElement('p');
+  para.innerText = todo;
+  todolist.appendChild(para)
+
+  para.addEventListener('click',()=>{
+    para.style.textDecoration = 'line-through'
+    remove(todo)
+  })
+  para.addEventListener('dblclick',()=>{
+    todolist.removeChild(para)
+    remove(todo)
+  })
+}
+
+function remove(todo) {
+ let index = todos.indexOf(todo)
+ if (index > -1) {
+  todos.splice(index,1)
+ }
+ localStorage.setItem('todos',JSON.stringify(todos))
+}
+
+});
